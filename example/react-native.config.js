b3b05b0e566eb70678f1c9f9140e84cd528ac3b9
huynq <huy.nguyen@jmango360.com>
2022-09-16 16:33:10 +0700

    chore: initial commit

---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-payments': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
---
+++ ios/Payments.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface Payments : NSObject <RCTBridgeModule>
+
+@end
